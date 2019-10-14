60efd9b68ec2429d1d675439c92be39a86a23657
xjinjin <13776572414@163.com>
2019-10-14 17:23:16 +0800

    test

--- cic_taxsb/models/models.py
+++ cic_taxsb/models/models.py
@@ -150,275 +150,6 @@ _val_and_maybe_convert.convertors = {
     'integer': int
 }
 
-class BalanceSheet(models.Model):
-    """资产负债表模板
-    模板用来定义最终输出的 资产负债表的格式,
-     每行的 科目的顺序 科目的大分类的所属的子科目的顺序
-    -- 本模板适合中国会计使用.
-    """
-
-    _name = "balance.sheet"
-    # _order = "sequence,id"
-    _description = '资产负债表模板'
-
-    # sequence = fields.Integer('序号') # 保留字段  可修改的排序，可以在列表视图里通过拖拽进行排序
-    line = fields.Integer('序号', required=True, help='资产负债表的行次') # 整行 ewbhxh
-    balance = fields.Char('资产')
-    # line_num = fields.Char('行次', help='此处行次并不是出报表的实际的行数,只是显示用的用来符合国人习惯')
-    ending_balance = fields.Float('期末余额')
-    # balance_formula = fields.Text(
-    #     '科目范围', help='设定本行的资产负债表的科目范围，例如1001~1012999999 结束科目尽可能大一些方便以后扩展')
-    beginning_balance = fields.Float('年初余额')
-
-    balance_two = fields.Char('负债和所有者权益')
-    # line_num_two = fields.Char('行次', help='此处行次并不是出报表的实际的行数,只是显示用的用来符合国人习惯')
-    ending_balance_two = fields.Float('期末余额')
-    # balance_two_formula = fields.Text(
-    #     '科目范围', help='设定本行的资产负债表的科目范围，例如1001~1012999999 结束科目尽可能大一些方便以后扩展')
-    beginning_balance_two = fields.Float('年初余额', help='报表行本年的年余额')
-    # company_id = fields.Many2one(
-    #     'res.company',
-    #     string='公司',
-    #     change_default=True,
-    #     default=lambda self: self.env['res.company']._company_default_get())
-
-class ProfitStatement(models.Model):
-    """利润表模板
-        模板主要用来定义项目的 科目范围,
-        然后根据科目的范围得到科目范围内的科目 的利润
-    """
-    _name = "profit.statement"
-    _order = "sequence,id" # 记录排序   在搜索的时候的默认排序，默认值是id
-    _description = '利润表模板'
-
-    # 保留字段  可修改的排序，可以在列表视图里通过拖拽进行排序
-    sequence = fields.Integer('序号')
-
-    balance = fields.Char('项目', help='报表的行次的总一个名称')
-    line_num = fields.Char('行次', help='生成报表的行次')
-    cumulative_occurrence_balance = fields.Float('本年累计金额', help='本年利润金额')
-    # 这一行第二个位置的公式
-    occurrence_balance_formula = fields.Text(
-        '科目范围', help='设定本行的利润的科目范围，例如1001~1012999999 结束科目尽可能大一些方便以后扩展')
-    current_occurrence_balance = fields.Float('本月金额', help='本月的利润的金额')
-    company_id = fields.Many2one(
-        'res.company',
-        string='公司',
-        change_default=True,
-        default=lambda self: self.env['res.company']._company_default_get())
-
-# TransientModel继承自Model，但是TransientModel的_transient = True
-class CreateBalanceSheetWizard(models.TransientModel):
-    """创建资产负债 和利润表的 wizard"""
-    _name = "create.balance.sheet.wizard"
-    _description = '资产负债表和利润表的向导'
-
-    company_id = fields.Many2one(
-        'res.company',
-        string='公司',
-        change_default=True, # change_default：别的字段的缺省值是否可依赖于本字段，缺省值为：False。
-        # odoo 中操作db的总句柄    # 利用 env[model] 获取模型类对象
-        default=lambda self: self.env['res.company']._company_default_get())
-
-    @api.model # self代表一个记录集合，但内容与记录集无关，只是与model类型相关。
-    def _default_period_domain(self):
-        """
-        用来设定期间的 可选的范围(这个是一个范围)
-        :return: domain条件
-        """
-        period_domain_setting = self.env['ir.values'].get_default(
-            'finance.config.settings', 'default_period_domain')
-        return [('is_closed', '!=', False)] if period_domain_setting == 'cannot' else []
-
-    @api.model
-    def _default_period_id(self):
-
-        return self._default_period_id_impl()
-
-    def _default_period_id_impl(self):
-        """
-                        默认是当前会计期间
-        :return: 当前会计期间的对象
-        """
-        return self.env['finance.period'].get_date_now_period_id()
-
-    # 通过self.period_id拿到的就是finance.period（1，）这条记录。domain：可选，用于在客户端筛选数据的domain表达式
-    period_id = fields.Many2one('finance.period', string='会计期间', domain=_default_period_domain,
-                                default=_default_period_id, help='用来设定报表的期间')
-
-    @api.multi
-    def compute_balance(self, parameter_str, period_id, compute_field_list):
-        """根据所填写的 科目的code 和计算的字段 进行计算对应的资产值"""
-        if parameter_str:
-            parameter_str_list = parameter_str.split('~')
-            subject_vals = []
-            if len(parameter_str_list) == 1:
-                subject_ids = self.env['finance.account'].search(
-                    [('code', '=', parameter_str_list[0])])
-            else:
-                subject_ids = self.env['finance.account'].search(
-                    [('code', '>=', parameter_str_list[0]), ('code', '<=', parameter_str_list[1])])
-            trial_balances = self.env['trial.balance'].search([('subject_name_id', 'in', [
-                                                              subject.id for subject in subject_ids]), ('period_id', '=', period_id.id)])
-            for trial_balance in trial_balances:
-                # 根据参数code 对应的科目的 方向 进行不同的操作
-                #  trial_balance.subject_name_id.costs_types == 'assets'解决：累计折旧 余额记贷方
-                if trial_balance.subject_name_id.costs_types == 'assets' or trial_balance.subject_name_id.costs_types == 'cost':
-                    subject_vals.append(
-                        trial_balance[compute_field_list[0]] - trial_balance[compute_field_list[1]])
-                elif trial_balance.subject_name_id.costs_types == 'debt' or trial_balance.subject_name_id.costs_types == 'equity':
-                    subject_vals.append(
-                        trial_balance[compute_field_list[1]] - trial_balance[compute_field_list[0]])
-            return sum(subject_vals)
-
-        else:
-            return 0
-
-    def deal_with_balance_formula(self, balance_formula, period_id, year_begain_field):
-        if balance_formula:
-            return_vals = sum([self.compute_balance(one_formula, period_id, year_begain_field)
-                               for one_formula in balance_formula.split(';')])
-        else:
-            return_vals = 0
-        return return_vals
-
-    def balance_sheet_create(self, balance_sheet_obj, year_begain_field, current_period_field):
-        balance_sheet_obj.write( # balance_sheet 单行的期末和年初值修改（负债表先调用完）
-            #  fabs() 方法返回数字的绝对值
-            {'beginning_balance': fabs(self.deal_with_balance_formula(balance_sheet_obj.balance_formula,
-                                                                      self.period_id, year_begain_field)),
-             'ending_balance': fabs(self.deal_with_balance_formula(balance_sheet_obj.balance_formula,
-                                                                   self.period_id, current_period_field)),
-             'beginning_balance_two': self.deal_with_balance_formula(balance_sheet_obj.balance_two_formula,
-                                                                     self.period_id, year_begain_field),
-             'ending_balance_two': self.deal_with_balance_formula(balance_sheet_obj.balance_two_formula,
-                                                                  self.period_id, current_period_field)})
-
-    @api.multi
-    def create_balance_sheet(self):
-        """ 资产负债表的创建 """
-        # balance_wizard = self.env['create.trial.balance.wizard'].create(
-        #     {'period_id': self.period_id.id}) # 新增一条记录
-        # balance_wizard.create_trial_balance()
-        # view_id = self.env.ref('finance.balance_sheet_tree_wizard').id # 获取XML的ID
-        balance_sheet_objs = self.env['balance.sheet'].search([]) # search:domain表达式，为空时返回所有记录
-        year_begain_field = ['year_init_debit', 'year_init_credit'] # 年初，资产、负债
-        current_period_field = [
-            'ending_balance_debit', 'ending_balance_credit'] # 期末，资产、负债
-        for balance_sheet_obj in balance_sheet_objs: # 资产负债表（先调用），每条记录对应一行，所有记录合成一整张表
-            self.balance_sheet_create( # 每行的期末和年初余额调用
-                balance_sheet_obj, year_begain_field, current_period_field)
-        force_company = self._context.get('force_company') #
-        if not force_company:
-            force_company = self.env.user.company_id.id
-        company_row = self.env['res.company'].browse(force_company)
-        days = calendar.monthrange(
-            int(self.period_id.year), int(self.period_id.month))[1]
-        attachment_information = '编制单位：' + company_row.name + ',,,,' + self.period_id.year\
-                                 + '年' + self.period_id.month + '月' + \
-            str(days) + '日' + ',,,' + '单位：元'
-        return {     # 返回生成资产负债表的数据的列表
-            'type': 'ir.actions.act_window',
-            'name': '资产负债表：' + self.period_id.name,
-            'view_type': 'form',
-            'view_mode': 'tree',
-            'res_model': 'balance.sheet',
-            'target': 'current',
-            'view_id': False,
-            'views': [(view_id, 'tree')],
-            'context': {'period_id': self.period_id.id, 'attachment_information': attachment_information},
-            'domain': [('id', 'in', [balance_sheet_obj.id for balance_sheet_obj in balance_sheet_objs])],
-            'limit': 65535,
-        }
-
-    def deal_with_profit_formula(self, occurrence_balance_formula, period_id, year_begain_field):
-        if occurrence_balance_formula:
-            return_vals = sum([self.compute_profit(balance_formula, period_id, year_begain_field)
-                               for balance_formula in occurrence_balance_formula.split(";")
-                               ])
-        else:
-            return_vals = 0
-        return return_vals
-
-    @api.multi
-    def create_profit_statement(self):
-        """生成利润表"""
-        balance_wizard = self.env['create.trial.balance.wizard'].create(
-            {'period_id': self.period_id.id})
-        balance_wizard.create_trial_balance()
-        view_id = self.env.ref('finance.profit_statement_tree').id
-        balance_sheet_objs = self.env['profit.statement'].search([])
-        year_begain_field = ['cumulative_occurrence_debit',
-                             'cumulative_occurrence_credit']
-        current_period_field = [
-            'current_occurrence_debit', 'current_occurrence_credit']
-        for balance_sheet_obj in balance_sheet_objs:
-            balance_sheet_obj.write({'cumulative_occurrence_balance': self.deal_with_profit_formula(balance_sheet_obj.occurrence_balance_formula, self.period_id, year_begain_field),
-                                     'current_occurrence_balance': self.compute_profit(balance_sheet_obj.occurrence_balance_formula, self.period_id, current_period_field)})
-        force_company = self._context.get('force_company')
-        if not force_company:
-            force_company = self.env.user.company_id.id
-        company_row = self.env['res.company'].browse(force_company)
-        days = calendar.monthrange(
-            int(self.period_id.year), int(self.period_id.month))[1]
-        attachment_information = '编制单位：' + company_row.name + ',,' + self.period_id.year\
-                                 + '年' + self.period_id.month + '月' + \
-            str(days) + '日' + ',' + '单位：元'
-        return {      # 返回生成利润表的数据的列表
-            'type': 'ir.actions.act_window',
-            'name': '利润表：' + self.period_id.name,
-            'view_type': 'form',
-            'view_mode': 'tree',
-            'res_model': 'profit.statement',
-            'target': 'current',
-            'view_id': False,
-            'views': [(view_id, 'tree')],
-            'context': {'period_id': self.period_id.id, 'attachment_information': attachment_information},
-            'domain': [('id', 'in', [balance_sheet_obj.id for balance_sheet_obj in balance_sheet_objs])],
-            'limit': 65535,
-        }
-
-    @api.multi
-    def compute_profit(self, parameter_str, period_id, compute_field_list):
-        """ 根据传进来的 的科目的code 进行利润表的计算 """
-        if parameter_str:
-            parameter_str_list = parameter_str.split('~')
-            subject_vals_in = []
-            subject_vals_out = []
-            total_sum = 0
-            sign_in = False
-            sign_out = False
-            if len(parameter_str_list) == 1:
-                subject_ids = self.env['finance.account'].search(
-                    [('code', '=', parameter_str_list[0])])
-            else:
-                subject_ids = self.env['finance.account'].search(
-                    [('code', '>=', parameter_str_list[0]), ('code', '<=', parameter_str_list[1])])
-            if subject_ids:   # 本行计算科目借贷方向
-                for line in subject_ids:
-                    if line.balance_directions == 'in':
-                        sign_in = True
-                    if line.balance_directions == 'out':
-                        sign_out = True
-            trial_balances = self.env['trial.balance'].search([('subject_name_id', 'in', [
-                                                              subject.id for subject in subject_ids]), ('period_id', '=', period_id.id)])
-            for trial_balance in trial_balances:
-                if trial_balance.subject_name_id.balance_directions == 'in':
-                    subject_vals_in.append(
-                        trial_balance[compute_field_list[0]])
-                elif trial_balance.subject_name_id.balance_directions == 'out':
-                    subject_vals_out.append(
-                        trial_balance[compute_field_list[1]])
-                if sign_out and sign_in:    # 方向有借且有贷
-                    total_sum = sum(subject_vals_out) - sum(subject_vals_in)
-                else:
-                    if subject_vals_in:
-                        total_sum = sum(subject_vals_in)
-                    else:
-                        total_sum = sum(subject_vals_out)
-            return total_sum
-
-
 class taxsbBase(models.Model):
     _name = "cic_taxsb.base"
     _description = "嘉商通总账系统申报接口API基础类"
@@ -886,6 +617,80 @@ class SBSubmit(models.Model):
     @api.onchange('sbzlbh')
     def _compute_serviceId(self):
         self.serviceId = self.sbzlbh + 'Submit'
+
+
+class ShenBaoSheet(models.Model):
+    """申报表模板
+    模板用来定义最终输出的 申报的报文格式,
+    分地区报表类型，利用定义每个单元格的模式来实现对任何种类表格的取数和生成报文需要
+    """
+
+    _name = "cic_taxsb.shenbaosheet"
+    _order = "sequence,id"
+    _description = '申报表模板'
+
+    name = fields.Char('名称')  # 一行
+    description = fields.Text('说明') # 说明
+    sequence = fields.Integer('序号') # 排序使用 _order
+    dqbm = fields.Selection(DQBM_SELECTION, string='地区编码', required=True, help='地区编码') # 分地区
+    tagname = fields.Char('报文标签') # tagname  是第二层的标签zcfzbGridlbVO   也就是一行的
+    cells = fields.One2many('cic_taxsb.shenbaosheet.cell', 'sheet_id', string='单元格设置') # 单元格设置
+    template = fields.Text('模板', help='备用的模板信息') # ？
+    # {'name': 'zcfzbGridlbVO', 'description': 'zcfzbGridlbVO', 'dqbm': '32', 'tagname': 'zcfzbGridlbVO'}
+    # {'name': 'zcfzbGridlbVO', 'description': 'zcfzbGridlbVO', 'dqbm': '32', 'tagname': 'zcfzbGridlbVO'}
+
+
+class ShenBaoCell(models.Model):
+    """申报表单元格定义模板
+    模板用来定义最终输出的 申报的报文格式,
+     单元格所在行号，名称，取数接口
+
+    """
+
+    _name = "cic_taxsb.shenbaosheet.cell"
+    _order = "sequence,id"
+    _description = '申报表单元格模板'
+
+    sheet_id = fields.Many2one('cic_taxsb.shenbaosheet', '申报表') #
+    sequence = fields.Integer('序号') # 排序
+    line = fields.Integer('行号', required=True, help='申报表的行') # 行号，一行两个行次
+    line_num = fields.Char('行次', help='此处行次并不是出报表的实际的行数,只是显示用的用来符合国人习惯') # 行次
+    tagname = fields.Char('报文标签') #
+    get_value_func = fields.Text('取值函数', help='设定本单元格的取数函数代码') #
+    # {'sheet_id': 1,  'line': 1, 'tagname': 'dqjkqmye'}
+
+class CreateShenbaoSheetWizard(models.TransientModel):
+    """创建申报报文的向导"""
+    _name = "create.shenbaosheet.wizard"
+    _description = '申报表的向导'
+
+    dqbm = fields.Selection(DQBM_SELECTION, string='地区编码', required=True, help='地区编码')
+    sheet_id = fields.Many2one('cic_taxsb.shenbaosheet', '申报表')
+    account_id = fields.Many2one('cic_ocr_report.account', '账套', help='对应总账系统的账套信息')
+    startdate = fields.Date('开始日期', help='开始日期')
+    enddate = fields.Date('截止日期', help='截止日期')
+    xml = fields.Text('XML报文')
+
+    @api.multi
+    def create_shenbao_sheet(self):
+        """ 申报表的创建 """
+        for record in self:
+            temp_dict = {}
+            for cell in record.sheet_id.cells:
+                if cell.line in temp_dict:
+                    key = cell.tagname
+                    value = exec(cell.get_value_func, locals={})
+                    if cell.line not in temp_dict:
+                        temp_dict[cell.line] = {'ewbhxh': cell.line}
+                    if key in temp_dict[cell.line]:
+                        if isinstance(temp_dict[cell.line][key], list):
+                            temp_dict[cell.line][key].append(value)
+                        else:
+                            temp_dict[cell.line][key] = [temp_dict[cell.line][key], value]
+                    else:
+                        temp_dict[cell.line][key] = value
+
+            record.xml = dict_to_xml({record.tagname: list(temp_dict.values())})
 
 # class SBXqykjzz(models.Model):
 #
